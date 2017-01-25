58530bd28f53605d2669763c8b4360f1cbad151a
Chris Marks <chrismarksus@gmail.com>
2017-01-24 21:08:58 -0500

    Fix #3 status chart color array has 4 colors

--- test/spec/charts/Status.js
+++ test/spec/charts/Status.js
@@ -13,7 +13,7 @@
     describe('The Status chart', () => {
       it('should set color to a progress array', function () {
         let result = chart.conf.colors;
-        expect(result).to.eql(['#e46c0a', '#376092', '#77933c']);
+        expect(result).to.eql(['#e46c0a', '#376092', '#77933c', '#c0504d']);
       });
       it('should have an mLabel of Cat', function () {
         let result = chart.mLabel;
